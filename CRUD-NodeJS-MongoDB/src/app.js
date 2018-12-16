797dcf364d182cbfd6f3fa74853d9c797ae35069
Kenedy Vargas <35229437+KaVargas@users.noreply.github.com>
2018-12-16 17:41:02 -0500

    Connection to MongoDB

--- CRUD-NodeJS-MongoDB/src/app.js
+++ CRUD-NodeJS-MongoDB/src/app.js
@@ -1,7 +1,31 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+//connecting to DB
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('Deb connected'))
+    .catch(err => console.log(err));
+
+//importing routes
+const indexRoutes = require('./routes/index');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname , 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+//routes
+app.use('/', indexRoutes);
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
 
